4aff8cc6b3af0dfb30f1537ab08126954998d0b1
MelRusenhack <melissa.rusenhack@gmail.com>
2023-09-05 19:58:39 -0300

    origin

---
+++ lista.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+    <title>Lista Cliente</title>
+</head>
+<div class="container">
+<body>
+    <h1> Lista Clientes </h1>
+    <hr>
+    <table class="table table-hover">
+
+        <thead>
+            <tr>
+            <th scope="col">#</th>
+            <th scope="col"> Nome </th>
+            <th scope="col"> Email </th>
+            <th scope="col"> Celular </th>
+            <th colspan="2"> Ações </th>
+            </tr>
+        </thead>
+
+        <tbody>
+            
+            <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>
+                <a href="#"><i class="bi bi-pencil-square"></i> Editar </a>
+            </td>
+
+            <td>
+                <a href="#"><i class="bi bi-trash"></i> Excluir </a>
+            </td>
+            </tr>
+
+            <tr>
+            <th scope="row">2</th>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+            <td>
+                <a href="#"><i class="bi bi-pencil-square"></i> Editar </a>
+            </td>
+            <td>
+                <a href="#"><i class="bi bi-trash"></i> Excluir </a>
+            </td>
+            </tr>
+
+            <tr>
+            <th scope="row">3</th>
+            <td colspan="2">Larry the Bird</td>
+            <td>@twitter</td>
+            <td>
+                <a href="#"><i class="bi bi-pencil-square"></i> Editar </a>
+            </td>
+            <td>
+                <a href="#"><i class="bi bi-trash"></i> Excluir </a>
+            </td>
+            </tr>
+
+        </tbody>
+    </table>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+<div>
+</html>
